21bd64214736aab8c47b2122f787ac9b7416f245
ravi@raviojha <raviojha477@gmail.com>
2020-03-07 07:28:48 -0800

    Updates

---
+++ precache-manifest.5b726934c7291d138ee81a2d5a42a1a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddb5923715a2d758a905e6ed52eb04d",
+    "url": "/pixabay-api-prod/index.html"
+  },
+  {
+    "revision": "705608d88883aaef5738",
+    "url": "/pixabay-api-prod/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "de902061df879f92d6ad",
+    "url": "/pixabay-api-prod/static/css/main.13a99e88.chunk.css"
+  },
+  {
+    "revision": "705608d88883aaef5738",
+    "url": "/pixabay-api-prod/static/js/2.ad8a46fb.chunk.js"
+  },
+  {
+    "revision": "6a984828c7bfd37325840f267d05c6ba",
+    "url": "/pixabay-api-prod/static/js/2.ad8a46fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de902061df879f92d6ad",
+    "url": "/pixabay-api-prod/static/js/main.96524989.chunk.js"
+  },
+  {
+    "revision": "5f77b2647a5513a319ed",
+    "url": "/pixabay-api-prod/static/js/runtime-main.5eff876e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pixabay-api-prod/static/media/logo.5d5d9eef.svg"
+  }
+]);
